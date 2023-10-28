45e2384bebc09357146d724073fecdc5abbfa100
ShamsiyyaAlakbarova <shamsnajafli@gmail.com>
2023-10-29 01:32:45 +0400

    sender/reader

--- reader.py
+++ reader.py
@@ -1,4 +1,4 @@
-import psycopg2
+'''import psycopg2
 import threading
 import time
 
@@ -33,4 +33,57 @@ sender_name = 'Shamsiyya'
 
 reader_thread = threading.Thread(target=read_available_messages, args=(sender_name,))
 reader_thread.start()
-reader_thread.join()
+reader_thread.join()'''
+
+
+import psycopg2
+import threading
+import time
+
+# Flag to control the reader thread
+terminate_reader = False
+
+def read_available_messages(sender_name):
+    global terminate_reader  # Allow the thread to access the flag
+
+    while not terminate_reader:
+        cursor.execute('SELECT SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME IS NULL AND SENDER_NAME = %s LIMIT 1',
+                       (sender_name,))
+        message = cursor.fetchone()
+
+        if message:
+            sender, message_text, sent_time = message
+            received_time = time.strftime('%Y-%m-%d %H:%M:%S')
+            print(f'Sender {sender} sent "{message_text}" at time {sent_time}')
+            cursor.execute('UPDATE ASYNC_MESSAGES SET RECEIVED_TIME = %s WHERE SENDER_NAME = %s AND RECEIVED_TIME IS NULL',
+                           (received_time, sender))
+            conn.commit()
+
+        time.sleep(1)  # Adjust the interval as needed
+
+# Create a PostgreSQL database connection
+conn = psycopg2.connect(
+    dbname='mydb',
+    user='postgres',
+    password='August2003',
+    host='127.0.0.1',
+    port='5432'
+)
+cursor = conn.cursor()
+
+# Define the sender_name variable or retrieve it from the sender
+sender_name = 'Shamsiyya'
+
+print("Reader is listening...")  # Display a listening message
+
+reader_thread = threading.Thread(target=read_available_messages, args=(sender_name,))
+reader_thread.start()
+
+# Allow the thread to run, but add an option to stop it
+print("Press 'q' and Enter to stop the receiver.")
+while not terminate_reader:
+    user_input = input()
+    if user_input.lower() == 'q':
+        terminate_reader = True
+
+reader_thread.join()  # Wait for the thread to finish
--- sender.py
+++ sender.py
@@ -1,4 +1,4 @@
-import psycopg2
+'''import psycopg2
 import threading
 import time
 import random
@@ -34,6 +34,46 @@ def user_input_loop():
         if user_input.lower() == 'exit':
             break
         random_thread = random.choice(threads)
+        message_thread = threading.Thread(target=send_message, args=(sender_name, user_input))
+        message_thread.start()
+
+user_input_thread = threading.Thread(target=user_input_loop)
+user_input_thread.start()
+user_input_thread.join()'''
+
+import psycopg2
+import threading
+import time
+import random
+
+def send_message(sender_name, message):
+    sent_time = time.strftime('%Y-%m-%d %H:%M:%S')
+    cursor.execute('INSERT INTO ASYNC_MESSAGES (SENDER_NAME, MESSAGE, SENT_TIME) VALUES (%s, %s, %s)',
+                   (sender_name, message, sent_time))
+    conn.commit()
+
+num_threads = 5  # Number of database servers
+threads = []
+
+# Create a PostgreSQL database connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="mydb",
+    user="postgres",
+    password="August2003")
+cursor = conn.cursor()
+
+for i in range(num_threads):
+    sender_name = 'Shamsiyya'
+    thread = threading.Thread(target=send_message, args=(sender_name, 'user_input'))
+    threads.append(thread)
+    thread.start()
+
+def user_input_loop():
+    while True:
+        user_input = input("Enter a message (or 'exit' to quit): ")
+        if user_input.lower() == 'exit':
+            break
         message_thread = threading.Thread(target=send_message, args=(sender_name, user_input))
         message_thread.start()
 
